0a3057bc24104b926acdcd87836a1de75bf037a1
nurmohammadrahul <nurrahul12@gmail.com>
2024-10-02 20:59:16 +0600

    final

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://solosphere.web.app',
+    'https://kormo1.netlify.app/',
   ],
   credentials: true,
   optionSuccessStatus: 200,
